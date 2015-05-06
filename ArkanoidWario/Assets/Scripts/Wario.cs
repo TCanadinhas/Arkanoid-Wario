3794cbe361b3d92c8a29969773bdd87e8975462c
12021999 <matolisou@gmail.com>
2015-05-06 15:06:38 -0300

    Arkanoid começado
    assets separados em pastas
    prefabs criados
    animações
    background criado
    mecânica começada

---
+++ ArkanoidWario/Assets/Scripts/Wario.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wario : MonoBehaviour {
+
+	public static bool collided = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Walls") 
+		{
+			print ("piru");
+			collided = true;
+			transform.position += new Vector3 (0.06f, 0.06f, 0);
+		} 
+		else 
+		{
+			collided = false;
+		}
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!collided) 
+		{
+			transform.position += new Vector3 (-0.06f, -0.06f, 0);
+		}
+	}
+}
